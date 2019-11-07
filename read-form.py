d96ce1538b09a7c796e5fc109985c1c61be9218f
ckhordiasma <ckhordiasma@gmail.com>
2019-11-06 20:07:26 -0600

    deleted test2.pdf for now. added a blank copy of 1206. made read-form work with beautifulsoup. safe to merge with master

--- read-form.py
+++ read-form.py
@@ -1,68 +1,62 @@
 
 import pikepdf
-import xml.dom.minidom
-fname = './af1206-e.pdf'
+
+from bs4 import BeautifulSoup
+fname = './af1206-blank.pdf'
 outfname = './test.pdf'
 bulletText = """first line
 test bullet 1
 test bullet 2
 refactoring in dictionaries
+refactoring in beautifulsoup
 """
+
+tagName = 'specificAccomplishments'
+xfaField = 'datasets'
 
 pdfobj = pikepdf.Pdf.open(fname,suppress_warnings=False)
 
 xfaroot = pdfobj.Root.AcroForm.XFA
 
-xmldata = ''
 xfaDict = {}
+xmlDict = {}
+
 for i,item in enumerate(xfaroot):
     if(i % 2 == 0 and isinstance(item,pikepdf.String)):
         #print(f'label {i}: {item}')
         label = f'{item}'
         xfaDict[label] = xfaroot[i+1]
+        if(isinstance(xfaDict[label],pikepdf.Stream)):
+            xmlSubstr = xfaDict[label].read_bytes().decode('utf-8')
+        else:
+            print('non stream detected')
+            xmlSubstr = xfaDict[label]
+        #print(xmlSubstr)
+        #some fields, like xmpmeta, are in some kind of binary and don't parse very well.
+        #xmlSubstr = re.sub(r'^b\'(.*)\'$',r'\1',xmlSubstr)
+        #xmlSubstr = re.sub(r'\\n',r'',xmlSubstr)
+        
+        xmlDict[label] = xmlSubstr
+        
     else:
         pass
         #print(f'value: {item.read_bytes()}')
 
-#print(xmlString)
 
-xmlDict = {}
-for key,item in xfaDict.items():
-    if(isinstance(item,pikepdf.Stream)):
-        xmlDict[key] = item.read_bytes()
-    else:
-        xmlDict[key] = item
+#html.parser seemed to work the best!
+formSoup = BeautifulSoup(xmlDict[xfaField],'xml')
+targetTag = formSoup.find(tagName)
 
-xmlFormData = xmlDict['datasets']
+targetTag.string = bulletText
 
-formdoc = xml.dom.minidom.parseString(xmlFormData)
-
-bulletTag = formdoc.getElementsByTagName('specificAccomplishments')[0]
-
-def setTagText(tag,text):
-    if(tag.hasChildNodes()):
-        tag.childNodes[0].nodeValue = text
-    else:
-        txtTag = tag.ownerDocument.createTextNode(text)
-        tag.appendChild(txtTag)
-
-def getTagText(tag,text):
-    if(tag.hasChildNodes()):
-        return tag.childNodes[0].nodeValue
-    else:
-        return 'None'
-
-setTagText(bulletTag,bulletText)
-
-
-#formdoc.documentElement hotfix is needed to omit <?xml version=...> tag at beginning of document
-xmlStringEdited = bytes(formdoc.documentElement.toxml(),'utf-8')
-#print(xmlStringEdited)
+#findchild() hotfix is needed to omit <?xml version=...> tag at beginning of document
+xmlStringEdited = bytes(str(formSoup.findChild()),'utf-8')
+print(xmlStringEdited)
 
 xfaDict['datasets'].write(xmlStringEdited)
 
 
 pdfobj.remove_unreferenced_resources()
 pdfobj.save(outfname,encryption=True)
-#pdfobj.close()
+pdfobj.close()
 
