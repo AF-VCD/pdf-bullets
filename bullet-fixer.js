0830e337fa31139f8ed4404ca3d2fd885564a57a
Christopher Kodama <chkodama@xoid.us>
2019-11-16 20:25:03 -0600

    implemented bullet class and bullet optimization dictionary idea... bullet optimization states (what width it's optimized for, what the actual sentence string is) are saved and updated as needed, instead of on every onInput of the text area. seems to work well as far as I can tell. the dictionary also removes old keys that aren't part of the textarea anymore.

--- bullet-fixer.js
+++ bullet-fixer.js
@@ -8,10 +8,8 @@ String.prototype.hashCode = function() {
     }
     return hash;
   };
-  
-function getRandomInt(seed,max){
-    return Math.floor( Math.abs((Math.floor(9*seed.hashCode()+5) % 100000) / 100000) * Math.floor(max));
-}
+
+
 function tokenize(sentence){
     words = sentence.split(/[\s]+/);
     //console.log(words);
@@ -25,71 +23,146 @@ function normalizeWhiteSpace(textAreaId){
         console.log("reformatted all white spaces")
         document.getElementById(textAreaId).value = cleaned;
 }
-function optimizeSpacings(spanNode){
 
-    var words = tokenize(spanNode.innerText);
-    
-    var smallerSpace = "\u2009";
-    var largerSpace = "\u2004";
 
-    var originalSentence = words.join(' '); 
-    spanNode.innerText = originalSentence;
-  
-    if(!originalSentence.trim()){return;}
-
-    //initial instantiation of previousSentence
-    var previousSentence = originalSentence;
-    
-    //This checks to see what the single line height of the span element is.
-    spanNode.style.whiteSpace = "nowrap"
-    singleHeight = spanNode.offsetHeight;
-    spanNode.style.whiteSpace = "inherit"
-
-    var addSpace = (spanNode.offsetHeight <= singleHeight);
-    
-    newSpace = addSpace? largerSpace: smallerSpace;
-    
-    console.log('Sentence: ' + originalSentence)
-    //console.log('\tspan node height: ' + spanNode.offsetHeight)
-    //console.log('\tdesired height: ' + singleHeight)
-    //console.log('\taddSpace: ' + addSpace)
-
-    while(true){
-
-       if(words.length <= 2){
-            console.log("\tWarning: Can't replace any more spaces");
-            spanNode.style.color = '#DC143C';
-            break;
-        }
-        
-        //don't select the first space after the dash- that would be noticeable and look wierd.
-        // also don't select the last word, don't want to add a space after that.
-        iReplace = getRandomInt(words.join(''), words.length -1 -1) + 1;
-        
-        //merges two elements together, joined by 
-        words.splice( 
-            iReplace, 2, 
-            words.slice(iReplace,iReplace+2).join(newSpace)
-        );
-        //console.log( (addSpace?'increased':'decreased') + ' space at index ' + iReplace + " : " + words[iReplace]);
-
-        //make all other spaces the normal space size
-        newSentence = words.join(' ');
-
-        //insert into the spanNode and see how its shape changes
-        spanNode.innerText = newSentence;
-        overflow = (spanNode.offsetHeight>singleHeight);
-
-        if(addSpace && overflow){            
-            //console.log("Warning: Can't add more spaces without overflow, reverting to previous" );
-            spanNode.innerText = previousSentence;
-            break;
-        
-        } else if(!addSpace && !overflow){
-            //console.log("Removed enough spaces. Terminating." );     
-            break;
-        }
-     
-        previousSentence = newSentence;
+function sentence2Key(sentence){
+    return tokenize(sentence).join(' ').trim().hashCode().toString();
+}
+function updateDict(textAreaId){
+    newBulletDict = {}
+    for (sentence of (document.getElementById(textAreaId)).value.split('\n')){
+        key = sentence2Key(sentence)
+        if(window.bulletDict[key]){
+            newBulletDict[key] = window.bulletDict[key];
+        }else{
+            newBulletDict[key] = {
+                'optimization':{}
+            };
+        }   
     }
+    //return bulletDict;
+    window.bulletDict = newBulletDict;
+}
+
+class Bullet{
+    constructor(bullet){
+        var bullet = bullet.trim();
+        if(! bullet){bullet = " ";}
+        this.words = tokenize(bullet);
+        this.optimization = {
+            "status": -1,
+            "sentence": this.words.join(""),
+            "width": "0 mm",
+        }
+    }
+    post(parent){
+        
+        var spanNode = document.createElement("span");
+        spanNode.className = "bullets";
+        spanNode.style.width = this.optimization.width;
+        spanNode.innerText = this.optimization.sentence;
+
+        if(this.optimization.status == 1){
+            spanNode.style.color = '#DC143C';
+        }else{
+            spanNode.style.color = 'black';
+        }
+
+        var divNode = document.createElement("div");
+        divNode.style.width = this.optimization.width;
+        
+        divNode.appendChild(spanNode);
+        parent.appendChild(divNode);
+
+    }
+    optimizeSpacings(width){
+        
+        var spanNode = document.createElement("span");
+        spanNode.className = "bullets";
+        spanNode.style.width = width;
+
+        var divNode = document.createElement("div");
+        divNode.appendChild(spanNode);
+        divNode.style.width = width;
+        //need to actually add it to the document to see how it fits
+        document.body.appendChild(divNode);
+
+        var smallerSpace = "\u2009";
+        var largerSpace = "\u2004";
+    
+        var originalSentence = this.words.join(' ');
+        spanNode.innerText = originalSentence;
+
+        //initialization of optimized words array
+        var optWords = this.words;
+        //initial instantiation of previousSentence
+        var previousSentence = originalSentence;
+        
+        //This checks to see what the single line height of the span element is.
+        spanNode.style.whiteSpace = "nowrap";
+        var singleHeight = spanNode.offsetHeight;
+        spanNode.style.whiteSpace = "inherit";
+    
+        var addSpace = (spanNode.offsetHeight <= singleHeight);
+        
+        var newSpace = addSpace? largerSpace: smallerSpace;
+        
+        console.log('Sentence: ' + originalSentence)
+        //console.log('\tspan node height: ' + spanNode.offsetHeight)
+        //console.log('\tdesired height: ' + singleHeight)
+        //console.log('\taddSpace: ' + addSpace)
+    
+        function getRandomInt(seed,max){
+            return Math.floor( Math.abs((Math.floor(9*seed.hashCode()+5) % 100000) / 100000) * Math.floor(max));
+        }
+
+        while(true){
+            //if the sentence is blank, do nothing.
+            if(!originalSentence.trim()){
+                this.optimization.status = 0;
+                break;
+            }
+           if(optWords.length <= 2){
+                console.log("\tWarning: Can't replace any more spaces");
+                this.optimization.status = 1;
+                break;
+            }
+
+            //don't select the first space after the dash- that would be noticeable and look wierd.
+            // also don't select the last word, don't want to add a space after that.
+            var iReplace = getRandomInt(optWords.join(''), optWords.length -1 -1) + 1;
+            
+            //merges two elements together, joined by 
+            optWords.splice( 
+                iReplace, 2, 
+                optWords.slice(iReplace,iReplace+2).join(newSpace)
+            );
+            //console.log( (addSpace?'increased':'decreased') + ' space at index ' + iReplace + " : " + optWords[iReplace]);
+    
+            //make all other spaces the normal space size
+            var newSentence = optWords.join(' ');
+    
+            //insert into the spanNode and see how its shape changes
+            spanNode.innerText = newSentence;
+            var overflow = (spanNode.offsetHeight>singleHeight);
+    
+            if(addSpace && overflow){            
+                //console.log("Warning: Can't add more spaces without overflow, reverting to previous" );
+                spanNode.innerText = previousSentence;
+                this.optimization.status = 0;
+                break;
+            
+            } else if(!addSpace && !overflow){
+                //console.log("Removed enough spaces. Terminating." );     
+                this.optimization.status = 0;
+                break;
+            }
+         
+            previousSentence = newSentence;
+        }
+        //at this point, spacings should be optimized as best as possible.
+        this.optimization.sentence = spanNode.innerText;
+        this.optimization.width = width;
+        divNode.remove();
+    }   
 }
