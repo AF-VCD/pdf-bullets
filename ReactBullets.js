ff1ba07f5b2128cd4f930304c75d8a7557190406
ckhordiasma <ckhordiasma@gmail.com>
2019-12-22 00:43:27 -0600

    changed where bullet optimization happens
    I made the optimization routines happen on a per-bullet basis. to implement that, I made a new class called OptimizedBullet that takes care of figuring out
    when to optimize and what to do once an optimization has been determined. However, the actual optimization routine is owned by the BulletComparator class.
    
    One thing that I found through googling is the concept of debouncing as it pertains to callbacks. if I debounce my key inputs (set a callback that is interrupted if the function is called again), it
     should eliminate the slowness problem I had been experiencing earlier.

--- ReactBullets.js
+++ ReactBullets.js
@@ -4,84 +4,21 @@ class Bullet extends React.Component{
     }
     render(){
         return(
-            <div className={this.props.class}>
-                {this.props.text}
+            <div style={{width: this.props.width}}>
+                <span className={this.props.class} style={this.props.style}>
+                    {this.props.text}
+                </span>
             </div>
         );
     }
-}
-
-
-class BulletsList extends React.Component{
-    constructor(props){
-        super(props);
-
-        const initialList = new DoublyLinkedList();
-
-        this.props.items.map((text) => {
-            const listNode = initialList.append(null, null);
-            listNode.data = this.create(text, listNode);
-        })
-        
-        this.state = { 
-            focusRef : initialList.head,
-            items: initialList
-        };
-       
+    static tokenize (sentence) {
+        return sentence.split(/[\s]+/);
     }
-    create = (bulletText, listNode) => {
-        return <BulletComparator 
-                    onKeyDown={(e) => (this.handleKeyDown(e))} 
-                    onFocus={(e) => (this.handleFocus(e,listNode))} 
-                    text={bulletText} 
-                    key={Math.random().toString()} 
-                    ref={React.createRef()} 
-                    />
-    }
-    
-    handleKeyDown = (e) => {
-        //console.log(this.state.BulletComparators);
-        const focusRef = this.state.focusRef;
-        console.log(focusRef)
-        if(e.keyCode == 38) {
-            //38 is UP
-            if(focusRef.prev != null){
-                //this.focusRefs[i-1].current.focus();
-                focusRef.prev.data.ref.current.focus();
-            }
-        }else if(e.keyCode == 40){
-            //40 is DOWN
-            if(focusRef.next != null){
-                focusRef.next.data.ref.current.focus();
-            }
-        } else if(e.keyCode == 13) {
-            this.setState((state)=>{
-                const listNode = state.items.append(null, focusRef);
-                listNode.data = this.create('', listNode);
-                return {
-                    items: state.items
-                };
-            },
-                () => {focusRef.next.data.ref.current.focus();}
-            );
-        }
-        // still need to implement: enter on a partial line, as well as delete on beginning of line
-    }
-    handleFocus = (e,ref) => {       
-        //this keeps track of what bullet is currently in focus
-        //console.log(ref)
-        this.setState({ 
-            focusRef: ref
-        });
-    }
-    render(){
-        return (
-            <div>
-                {Array.from(this.state.items.get())}
-            </div>
-        )
+    static clean(sentence){
+        return Bullet.tokenize(sentence).join(' ');
     }
 }
+
 class BulletEditor extends React.Component{
     constructor(props){
         super(props);
@@ -91,8 +28,12 @@ class BulletEditor extends React.Component{
     }
     render(){
         return (
-            <div>
-                <textarea onChange={this.handleChange} value={this.props.text}></textarea>
+            <div className="bulletContainer border" >
+                <textarea 
+                    onChange={this.handleChange} 
+                    value={this.props.text} 
+                    style={{width: this.props.width}}
+                    className="bullets"></textarea>
             </div>
         )
     }
@@ -100,47 +41,147 @@ class BulletEditor extends React.Component{
 //how do i get lines to line up between the output and editor?
 class BulletOutputViewer extends React.Component{
     constructor(props){
-        super(props)
+        super(props);
     }
-    optimize = (text) => {
-        return text + "!!!!!";
-    }
+    
     render(){
         return (
-            <div>
-                {this.props.text.split('\n').map(
+            <div className="bulletContainer border">
+                {this.props.bullets.map(
                 (line,i)=>{
-                    const lineOpt = this.optimize(line)
-                    return <Bullet text={lineOpt} key={i+line} class='bullet optimized' />
+                    return <OptimizedBullet text={line} 
+                        width={this.props.width}
+                        key={i+Bullet.tokenize(line).join(" ")} 
+                        class='bullet optimized' 
+                        optims={this.props.optims}
+                        onOptim={this.props.onOptim}
+                        optimizer={this.props.optimizer}/>
                 })}
             </div>
         )
     }
 
 }
+class OptimizedBullet extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text: this.props.text,
+            loading: true
+        }
+        console.log("reconstructed " + this.props.text)
+    }
+    alreadyOptim = (sentence) => {
+        if(this.props.optims[sentence] && this.props.optims[sentence][this.props.width]){
+            return true
+        }else{
+            return false
+        }
+    }
+    update = (sentence) => {
+        if(this.alreadyOptim(sentence)){
+            if(this.state.text !=  this.props.optims[sentence][this.props.width]){
+                this.setState({
+                    text: this.props.optims[sentence][this.props.width],
+                    loading:false
+                });
+            }else{
+                this.setState({loading:false})
+            }
+        }else{
+            console.log('Optimization not found (yet)')
+            this.setState({
+                text: this.props.text,
+                loading:true
+            })
+            this.optimize(sentence);
+        }
+    }
+    optimize = (sentence) => {
+        sentence = Bullet.clean(sentence);
+        return this.props.optimizer(sentence).then((optimized) => {
+            this.props.onOptim({
+                "sentence":sentence,
+                "width": this.props.width,
+                "optimized": optimized});
+            return optimized;
+        }).then((sentence) => {
+            this.setState({
+                text:sentence,
+                loading:false
+            })
+        })
+        
+    }
+  
+    componentDidMount(){
+        console.log('component mounted: ' + this.props.text)
+        const slowUpdate = debounced(500, this.update)
+        slowUpdate(this.props.text)
+        //this.update(this.props.text);
+    }
+    
+    render(){
+        //console.log(this.props)
+        return (
+            <Bullet text={this.state.text} 
+                        width={this.props.width} 
+                        class='bullet optimized' style={this.state.loading ? {color:"gray"}: {}}/>
+        )
+    }
 
+}
 class BulletComparator extends Bullet {
     constructor(props){
         super(props);
         
         this.state = { 
             text: this.props.initialText,
+            optims: {}
         };
-        
-    }
-    optimize(sentence){
-        return sentence + '!';
-    }
-    handleTextChange = (e) => {
-        this.setState({
-            text: e.target.value
+
+        this.state.text.split('\n').map((line) => {
+            this.state.optims[line] = this.state.optims[line] || {};
+            this.optimize(line).then((optimized)=>{
+                this.state.optims[line][this.props.width]=optimized
+            });
         })
+
+    }
+
+    optimize = (sentence) => {
+        return new Promise((res)=>{
+            let i=0;
+            for(let j=0;j<100000;j++){
+                i += Math.random();
+            }
+            res(sentence+'!!!!'+i);
+        });
+    }
+    updateOptims = (params) => {
+        this.setState((state) => {
+            state.optims[params.sentence] = state.optims[params.sentence] || {};
+            state.optims[params.sentence][params.width] = params.optimized;
+            return state
+        });
+    }
+
+    handleTextChange = (e) => {
+        
+        this.setState({
+            text: e.target.value,
+        });
     }
     render() {
+        //console.log(this.state)
         return (
             <div>
-                <BulletEditor text={this.state.text} handleTextChange={this.handleTextChange}/>
-                <BulletOutputViewer text={this.state.text} handleTextChange={this.handleTextChange} />
+                <BulletEditor text={this.state.text} handleTextChange={this.handleTextChange} width={this.props.width}/>
+                <BulletOutputViewer bullets={this.state.text.split('\n')} 
+                    handleTextChange={this.handleTextChange}  width={this.props.width} 
+                    optims={this.state.optims} 
+                    optimizer={this.optimize}
+                    onOptim={this.updateOptims}/>
             </div>
         );
     }
@@ -150,5 +191,5 @@ const data = '- Saved Air Force moneys \n\
 - Engineered 900 airplanes';
 
 ReactDOM.render(
-    <BulletComparator initialText={data} />, document.getElementById('stuff')
+    <BulletComparator initialText={data} width="202.51mm"/>, document.getElementById('stuff')
     );
--- utils.js
+++ utils.js
@@ -64,3 +64,17 @@ class DoublyLinkedList {
        this.prev = null;
     }
  };
+
+
+ debounced= (delay, fn) => {
+   let timerId;
+   return function (...args) {
+       if (timerId) {
+       clearTimeout(timerId);
+       }
+       timerId = setTimeout(() => {
+       fn(...args);
+       timerId = null;
+       }, delay);
+   }
+}
