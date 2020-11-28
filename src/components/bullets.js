1101c17bc63075f2205573ce8dbe61f6988097f6
Christopher Kodama <ckhordiasma@gmail.com>
2020-11-28 00:37:38 -0500

    commented renderBulletText and tweaked if logic

--- src/components/bullets.js
+++ src/components/bullets.js
@@ -1,8 +1,8 @@
 import React from "react"
-import {Editor, EditorState, RichUtils, ContentState, Modifier} from "draft-js"
+import { Editor, EditorState, RichUtils, ContentState, Modifier } from "draft-js"
 import "draft-js/dist/Draft.css";
 import { toSetSeq } from "draft-js/lib/DefaultDraftBlockRenderMap";
-import {getSelectionInfo} from './tools.js'
+import { getSelectionInfo } from './tools.js'
 const DPI = 96;
 const MM_PER_IN = 25.4;
 const DPMM = DPI / MM_PER_IN;
@@ -24,14 +24,14 @@ const BULLET = {
 }
 
 
-function BulletComparator({editorState, setEditorState, width, ...props}){
-    
+function BulletComparator({ editorState, setEditorState, width, ...props }) {
+
     const bulletOutputID = "bulletOutput";
 
     // Editor callback that adds rich text editor keybinds
     const handleKeyCommand = (command, editorState) => {
         const newState = RichUtils.handleKeyCommand(editorState, command);
-        if(newState){
+        if (newState) {
             setEditorState(newState);
             return 'handled';
         }
@@ -39,10 +39,10 @@ function BulletComparator({editorState, setEditorState, width, ...props}){
     }
 
     // Editor callback that runs whenever edits or selection changes occur.
-    const onChange = (newEditorState)=>{
-        
+    const onChange = (newEditorState) => {
+
         const textChanged = editorState.getCurrentContent() !== newEditorState.getCurrentContent();
-        
+
         //const content = editorState.getCurrentContent();
         // ordered map has a key and a block associasted with it
         //const blockMap = content.getBlockMap();
@@ -51,30 +51,30 @@ function BulletComparator({editorState, setEditorState, width, ...props}){
             console.log(block.getText());
         }
         */
-        const {selectedText} = getSelectionInfo(newEditorState)
-        if(props.onSelect && selectedText !== '') props.onSelect(selectedText);
-        if(props.onTextChange && textChanged) props.handleTextChange(newEditorState.getCurrentContent().getPlainText('\n'))
+        const { selectedText } = getSelectionInfo(newEditorState)
+        if (props.onSelect && selectedText !== '') props.onSelect(selectedText);
+        if (props.onTextChange && textChanged) props.handleTextChange(newEditorState.getCurrentContent().getPlainText('\n'))
         setEditorState(newEditorState);
     }
 
     // This other bullet selection is for when things are selected on the optimized output
-    const onBulletSelect = (event)=>{
+    const onBulletSelect = (event) => {
         const selection = window.getSelection().toString();
-        if(selection !== ""){
+        if (selection !== "") {
             props.onSelect(selection)
         }
     }
 
     // TODO add control-a selectability on the bullet outputs
 
-    
-    function selectOutput (e){
 
-        if(e.ctrlKey && e.keyCode === 65){
+    function selectOutput(e) {
+
+        if (e.ctrlKey && e.keyCode === 65) {
             e.preventDefault();
             //console.log('control-a')
             //console.log(this.outputRef.current)
-            if (e.target.id.match(new RegExp(bulletOutputID))) { 
+            if (e.target.id.match(new RegExp(bulletOutputID))) {
                 const range = document.createRange();
                 range.selectNode(e.target);
                 window.getSelection().removeAllRanges();
@@ -82,27 +82,27 @@ function BulletComparator({editorState, setEditorState, width, ...props}){
             }
         }
     }
-    
+
 
     return (
         <div className="bullets columns is-multiline" >
             <div className="column" style={{
-                width: width+'mm',
-                }}>
+                width: width + 'mm',
+            }}>
                 <h2 className='subtitle'>Input Bullets Here:</h2>
-                <div className="border" style={{maxWidth: width*1.01+'mm'}}>
-                    <Editor className="border" 
-                    editorState={editorState} onChange={onChange} handleKeyCommand={handleKeyCommand}/>
+                <div className="border" style={{ maxWidth: width * 1.01 + 'mm' }}>
+                    <Editor className="border"
+                        editorState={editorState} onChange={onChange} handleKeyCommand={handleKeyCommand} />
                 </div>
             </div>
-            <div className="column" id={bulletOutputID} style={{width: width+'mm'}}
+            <div className="column" id={bulletOutputID} style={{ width: width + 'mm' }}
                 onMouseUp={onBulletSelect} onKeyDown={selectOutput} tabIndex="0">
                 <h2 className='subtitle'>View Output Here:</h2>
                 <div className="border">
-                    {editorState.getCurrentContent().getBlocksAsArray().map((block, key)=>{
+                    {editorState.getCurrentContent().getBlocksAsArray().map((block, key) => {
                         let text = block.getText();
-                        if(props.abbrReplacer) text = props.abbrReplacer(text);
-                        return <Bullet text={text} widthPx={width*DPMM} enableOptim={props.enableOptim}/>
+                        if (props.abbrReplacer) text = props.abbrReplacer(text);
+                        return <Bullet text={text} widthPx={width * DPMM} enableOptim={props.enableOptim} />
                     })}
                 </div>
             </div>
@@ -120,16 +120,16 @@ function BulletComparator({editorState, setEditorState, width, ...props}){
 
 
 
-function Bullet({text, widthPx, ...props}){
+function Bullet({ text, widthPx, ...props }) {
     const canvasRef = React.useRef(null);
     const [outputText, setOutputText] = React.useState(' ');
-    
+
     const [color, setColor] = React.useState('inherit');
     const [loading, setLoading] = React.useState(true);
-    const [optimStatus, setOptimStatus] = React.useState(BULLET.NOT_OPT);    
-    const [rendering, setBulletRendering] = React.useState({text:''});
+    const [optimStatus, setOptimStatus] = React.useState(BULLET.NOT_OPT);
+    const [rendering, setBulletRendering] = React.useState({ text: '' });
 
-    const getContext = (canvas)=>{
+    const getContext = (canvas) => {
         //now we can draw in 2d here.
         const context = canvas.getContext('2d');
         context.font = '12pt AdobeTimes';
@@ -139,7 +139,7 @@ function Bullet({text, widthPx, ...props}){
     // This effect updates the text rendering (i.e. enforces width constraints by inserting newlines)
     //   whenever the props text input is updated.
     React.useEffect(() => {
-        
+
         const context = getContext(canvasRef.current)
         //context.fillText(text, 50,50);
         setBulletRendering(renderBulletText(text, context, widthPx));
@@ -150,32 +150,32 @@ function Bullet({text, widthPx, ...props}){
 
     // This effect happens after bullet rendering changes. It evaluates the rendered bullet and
     //  sees how it can be improved with modified spaces. 
-    React.useEffect(()=>{
-        
-        if(props.enableOptim){
+    React.useEffect(() => {
+
+        if (props.enableOptim) {
             setLoading(BULLET.LOADING);
-            const optimizer = (text)=>renderBulletText(text, getContext(canvasRef.current),widthPx);
+            const optimizer = (text) => renderBulletText(text, getContext(canvasRef.current), widthPx);
             const optimResults = optimize(rendering.text, optimizer);
-    
+
             setLoading(BULLET.DONE);
-    
+
             setOptimStatus(optimResults.status);
-            setOutputText(optimResults.rendering.text);  
-               
-        }else{
-            setOutputText(rendering.text);   
+            setOutputText(optimResults.rendering.text);
+
+        } else {
+            setOutputText(rendering.text);
         }
 
-        
-    },[rendering]);
+
+    }, [rendering]);
 
     //color effect
-    React.useEffect(()=>{
-        if(loading){
+    React.useEffect(() => {
+        if (loading) {
             setColor("gray")
-        }else if(optimStatus === BULLET.FAILED_OPT){
+        } else if (optimStatus === BULLET.FAILED_OPT) {
             setColor("red");
-        }else{
+        } else {
             setColor("inherit");
         }
     }, [loading, outputText])
@@ -184,21 +184,21 @@ function Bullet({text, widthPx, ...props}){
     //whitespace: pre-wrap is essential as it allows javascript string line breaks to appear properly.
     return (
         <>
-        <canvas 
-            ref={canvasRef} 
-            style={{
-                visibility:"hidden", 
-                position:"absolute",
-                top:"-1000px",
-                left: "-1000px"   
-        }}/>
-        <div style={{
-            height:props.height,
-            whiteSpace: 'pre-wrap',
-            color: color,
+            <canvas
+                ref={canvasRef}
+                style={{
+                    visibility: "hidden",
+                    position: "absolute",
+                    top: "-1000px",
+                    left: "-1000px"
+                }} />
+            <div style={{
+                height: props.height,
+                whiteSpace: 'pre-wrap',
+                color: color,
             }} onMouseUp={props.onHighlight} >
-            {outputText == '' ? ' ' : outputText}
-        </div>
+                {outputText == '' ? ' ' : outputText}
+            </div>
         </>
     );
     //return canvas;
@@ -206,165 +206,171 @@ function Bullet({text, widthPx, ...props}){
 
 
 function optimize(sentence, evalFcn) {
-    
+
     const smallerSpace = "\u2006";
     const largerSpace = "\u2004";
 
     //initialization of optimized words array
     let optWords = BULLET.Tokenize(sentence);
-    
+
     const initResults = evalFcn(sentence);
-    
-    if(initResults.overflow == 0){
+
+    if (initResults.overflow == 0) {
         return initResults;
     }
 
     //initial instantiation of previousResults
     let prevResults = initResults;
     let finalResults = initResults;
-    const newSpace = (initResults.overflow >= 0 )? smallerSpace: largerSpace;
-    
+    const newSpace = (initResults.overflow >= 0) ? smallerSpace : largerSpace;
+
     let finalOptimStatus = BULLET.NOT_OPT;
-    
-    function getRandomInt(seed,max){
-        return Math.floor( Math.abs((Math.floor(9*seed.hashCode()+5) % 100000) / 100000) * Math.floor(max));
+
+    function getRandomInt(seed, max) {
+        return Math.floor(Math.abs((Math.floor(9 * seed.hashCode() + 5) % 100000) / 100000) * Math.floor(max));
     }
-    
-    
-    while(finalResults.overflow > 0 || finalResults.overflow < BULLET.MAX_UNDERFLOW){
+
+
+    while (finalResults.overflow > 0 || finalResults.overflow < BULLET.MAX_UNDERFLOW) {
         //don't select the first space after the dash- that would be noticeable and look wierd.
         // also don't select the last word, don't want to add a space after that.
-        let iReplace = getRandomInt(optWords.join(''), optWords.length -1 -1) + 1;
-        
+        let iReplace = getRandomInt(optWords.join(''), optWords.length - 1 - 1) + 1;
+
         //merges two elements together, joined by the space
-        optWords.splice( 
-            iReplace, 2, 
-            optWords.slice(iReplace,iReplace+2).join(newSpace)
+        optWords.splice(
+            iReplace, 2,
+            optWords.slice(iReplace, iReplace + 2).join(newSpace)
         );
 
         //make all other spaces the normal space size
         let newSentence = optWords.join(' ');
-        
+
         let newResults = evalFcn(newSentence);
 
-        if(initResults.overflow <= 0 && newResults.overflow > 0){            
+        if (initResults.overflow <= 0 && newResults.overflow > 0) {
             //console.log("Note: Can't add more spaces without overflow, reverting to previous" );
             finalResults = prevResults;
             finalOptimStatus = BULLET.OPTIMIZED;
             break;
-        } else if(initResults.overflow > 0  && newResults.overflow < 0){
+        } else if (initResults.overflow > 0 && newResults.overflow < 0) {
             //console.log("Removed enough spaces. Terminating." );
             finalResults = newResults;
             finalOptimStatus = BULLET.OPTIMIZED;
             break;
-        } else if(optWords.length <= 2){ //this conditional needs to be last
+        } else if (optWords.length <= 2) { //this conditional needs to be last
             //console.log("\tWarning: Can't replace any more spaces");
             finalResults = newResults;
             finalOptimStatus = BULLET.FAILED_OPT;
             break;
         }
         prevResults = newResults;
-    } 
+    }
     return {
         status: finalOptimStatus,
         rendering: finalResults,
     };
 }
-        
+
 // all widths in this function are in pixels
-function renderBulletText(text, context, width){
-    
-    const getWidth = (txt)=> (context.measureText(txt)).width;
+function renderBulletText(text, context, width) {
+
+    const getWidth = (txt) => (context.measureText(txt)).width;
     const fullWidth = getWidth(text.trimEnd());
-    if(fullWidth < width){
+    if (fullWidth < width) {
         return {
             text: text,
             fullWidth: fullWidth,
             lines: 1,
             overflow: fullWidth - width,
         };
-    }else{
+    } else {
         // Scenario where the width of the text is wider than desired.
         //  In this case, work needs to be done to figure out where the line breaks should be. 
 
         // Regex- split after one of the following: \s ? / | - % ! 
         // but ONLY if immediately followed by: [a-zA-z] [0-9] + \
-        const textSplit = text.split(/(?<=[\s\?\/\|\-\%\!])(?=[a-zA-Z0-9\+\\])/) ;
-        
-        if(getWidth(textSplit[0]) < width){
+        const textSplit = text.split(/(?<=[\s\?\/\|\-\%\!])(?=[a-zA-Z0-9\+\\])/);
+
+        // check to make sure the first token is smaller than the desired width.
+        //   This is usually true, unless the desired width is abnormally small, or the 
+        //   input text is one really long word
+        if (getWidth(textSplit[0]) < width) {
             let answerIdx = 0;
-            for(let i = 1; i <= textSplit.length; i++){
-                const evalText = textSplit.slice(0,i).join('').trimEnd();
+            for (let i = 1; i <= textSplit.length; i++) {
+                const evalText = textSplit.slice(0, i).join('').trimEnd();
                 const evalWidth = getWidth(evalText);
-                if(evalWidth > width){
+                if (evalWidth > width) {
                     answerIdx = i - 1;
                     break;
                 }
             }
-            const recursedText = textSplit.slice(answerIdx,textSplit.length).join('');
+            const recursedText = textSplit.slice(answerIdx, textSplit.length).join('');
 
-            if(recursedText == text){
+            if (recursedText == text) {
                 console.warn("Can't fit \"" + text + "\" on a single line");
                 return {
-                    text, 
+                    text,
                     fullWidth,
                     lines: 1,
                     overflow: fullWidth - width,
                 };
+            } else {
+                const recursedResult = renderBulletText(recursedText, context, width);
+
+                return {
+                    text: textSplit.slice(0, answerIdx).join('') + '\n' + recursedResult.text,
+                    fullWidth: fullWidth,
+                    lines: 1 + recursedResult.lines,
+                    overflow: fullWidth - width,
+                }
             }
-            const recursedResult = renderBulletText(recursedText, context, width);
-            
-            return {
-                text: textSplit.slice(0,answerIdx).join('') + '\n' + recursedResult.text,
-                fullWidth: fullWidth,
-                lines: 1 + recursedResult.lines,
-                overflow: fullWidth - width,
-            }
-        }else{
-            //corner case where text is one long string without spaces in it.
-            const avgCharWidth = fullWidth/(text.length);
+
+        } else {
+
+            const avgCharWidth = fullWidth / (text.length);
             const guessIndex = parseInt(width / avgCharWidth);
-            const firstGuessWidth = getWidth(text.substring(0,guessIndex)) 
+            const firstGuessWidth = getWidth(text.substring(0, guessIndex))
             let answerIdx = guessIndex;
-            if(firstGuessWidth > width){
-                for(let i=guessIndex-1; i>0; i--){
-                    const nextGuessWidth = getWidth(text.substring(0,i));
-                    if(nextGuessWidth < width){
+            if (firstGuessWidth > width) {
+                for (let i = guessIndex - 1; i > 0; i--) {
+                    const nextGuessWidth = getWidth(text.substring(0, i));
+                    if (nextGuessWidth < width) {
                         answerIdx = i;
                         break;
                     }
                 }
-            }else if(firstGuessWidth < width){
-                for(let i=guessIndex; i <= text.length; i++){
-                    
-                    const nextGuessWidth = getWidth(text.substring(0,i));
-                    if(nextGuessWidth > width){
+            } else if (firstGuessWidth < width) {
+                for (let i = guessIndex; i <= text.length; i++) {
+
+                    const nextGuessWidth = getWidth(text.substring(0, i));
+                    if (nextGuessWidth > width) {
                         answerIdx = i - 1;
                         break;
                     }
                 }
             }
-            const recursedText = text.substring(answerIdx,text.length);
-            if(recursedText == text){
+            const recursedText = text.substring(answerIdx, text.length);
+            if (recursedText == text) {
                 console.warn("Could not even fit first character of \"" + text + "\" on a single line");
                 return {
-                    text, 
+                    text,
                     fullWidth,
                     lines: 1,
                     overflow: fullWidth - width
                 };
-            }
-            const recursedResult = renderBulletText(recursedText, context, width);
-            
-            return {
-                text: text.substring(0,answerIdx) + '\n' + recursedResult.text,
-                fullWidth: fullWidth,
-                lines: 1 + recursedResult.lines,
-                overflow: fullWidth - width,
+            } else {
+                const recursedResult = renderBulletText(recursedText, context, width);
+
+                return {
+                    text: text.substring(0, answerIdx) + '\n' + recursedResult.text,
+                    fullWidth: fullWidth,
+                    lines: 1 + recursedResult.lines,
+                    overflow: fullWidth - width,
+                }
             }
         }
     }
 }
 
 
-export {Bullet, BULLET, BulletComparator, renderBulletText};
+export { Bullet, BULLET, BulletComparator, renderBulletText };
