d7953c5140a6753ee70ec26cf5c6dd70b166d61a
Christopher Kodama <ckhordiasma@gmail.com>
2020-12-15 22:15:49 -0500

    added lots more test cases to check edge cases

--- src/tests/bullet.render.test.js
+++ src/tests/bullet.render.test.js
@@ -1,6 +1,7 @@
 import widthMap from  "./12pt-times.json"
 import {renderBulletText} from '../components/tools.js';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
 // add custom jest matchers from jest-dom
 
@@ -48,11 +49,27 @@ function RenderBulletTextTester(props){
     //return canvas;
 }
 
-describe('checking bullets', () => {
+let container = null;
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // cleanup on exiting
+    ReactDOM.unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+describe('checking AF1206 bullets', () => {
+    const width = 202.321 * DPMM ;
+
     it('evaluates various bullets properly', ()=>{
 
         let bullets = [];
-        const width = 202.321 * DPMM;
+        
         
 
         bullets.push({
@@ -81,13 +98,6 @@ describe('checking bullets', () => {
             answer: 'asdfasdfjkalsdjfioasdfjioasdfas8df908asdfuas89djf89ashdfiasndfjinasdf9jas890dfj90asdf90as90df90asdf90asdf90asdf9\n9999999'
         });
 
-        
-        bullets.push({
-            id:'example1',
-            input: 'Led 42 mbrs/3 AFSCs thru JQS update; certified mbrs/trainers on 133 rqmts--propelled nascent flight trng to wing std',
-            answer: 'Led 42 mbrs/3 AFSCs thru JQS update; certified mbrs/trainers on 133 rqmts--propelled nascent flight trng to wing std'
-        });
-
         bullets.push({
             id:"long1",
             input:"this bullet is really long and definitely should have multiple line breaks-- chicken duck air force cyber EW space force chair force computer error seven swords zelda mario turtle frog dinosaur zebra disaster asteroid aliens frog chicken  hello world after chicken birthday party computer error seven swords zelda mario turtle frog dinosaur zebra disaster asteroid aliens frog chicken",
@@ -99,11 +109,68 @@ describe('checking bullets', () => {
                 {bullets.map((entry)=>{
                     return < RenderBulletTextTester key={entry.id} id={entry.id} text={entry.input} width={width} />
                 })}
-            </>);
+            </>, container);
 
         for (let entry of bullets){
             expect(screen.getByTestId(entry.id)).toHaveTextContent(entry.answer, {normalizeWhitespace:false});
         }
         
     });
+
+    it('correctly renders pre-optimized and/or perfect bullets (verified to fit in adobe pdf)', ()=>{
+        const optimized = (
+`- Led 42 mbrs/3 AFSCs thru JQS update; certified mbrs/trainers on 133 rqmts--propelled nascent flight trng to wing std
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+- aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+- Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint oc
+- amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mi
+- minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure d
+- dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidata
+- non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+- adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+- nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+- in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+- aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+- Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint oc
+- occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+- amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mi
+- minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure d
+- dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidata
+- non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+- nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+- in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in`);
+
+        optimized.split('\n').forEach((line, i)=>{
+            render(< RenderBulletTextTester key={i} id={i}  text={line} width={width} />, container);
+            ////expect(screen.getByTestId(i+'').textContent.split('\n').length).toEqual(1);
+            expect(screen.getByTestId(i+'')).toHaveTextContent(line, {normalizeWhitespace:false});
+        })
+    })
+})
+
+
+describe('checking AF707 bullets', () => {
+    const width = 201.041 * DPMM;
+
+    it('correctly renders pre-optimized and/or perfect bullets (verified to fit in adobe pdf)', ()=>{
+        const optimized = (
+`- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+- aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+- Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint oc
+- occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+- amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mi
+- minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure d
+- dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidata
+- non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+- adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+- nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+- in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in`);
+
+        optimized.split('\n').forEach((line, i)=>{
+            render(< RenderBulletTextTester key={i} id={i}  text={line} width={width} />, container);
+            ////expect(screen.getByTestId(i+'').textContent.split('\n').length).toEqual(1);
+            expect(screen.getByTestId(i+'')).toHaveTextContent(line, {normalizeWhitespace:false});
+        })
+    })
 })
