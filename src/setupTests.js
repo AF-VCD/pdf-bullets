4b6a9c5f1fad09ee23b12bf38317186772426bcc
Christopher Kodama <ckhordiasma@gmail.com>
2021-01-24 18:30:26 -0500

    updated testing related dependencies

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,4 +3,5 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
-import 'jest-canvas-mock'
+import 'jest-canvas-mock'; // needed for Bullet tests
+import 'regenerator-runtime/runtime'; // needed for AbbrTable.test.js
