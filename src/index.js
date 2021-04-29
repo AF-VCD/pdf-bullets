6c9ca338c02b5d41293375c8b19f273d19b74296
Christopher Kodama <ckhordiasma@gmail.com>
2021-04-29 00:17:02 -0400

    testing git push on gitlab-- insignificant change

--- src/index.js
+++ src/index.js
@@ -1,43 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 //import {Bullet, BulletComparator, Skeleton} from "./components/bullets.js"
-import BulletApp from "./BulletApp.js"
+import BulletApp from "./BulletApp.js";
 
-import "./styles/index.scss"
-import 'handsontable/dist/handsontable.full.css'
+import "./styles/index.scss";
+import "handsontable/dist/handsontable.full.css";
 
-import * as serviceWorker from './serviceWorker';
-import WebFont from 'webfontloader';
-
-
+import * as serviceWorker from "./serviceWorker";
+import WebFont from "webfontloader";
 
 WebFont.load({
   custom: {
-    families: ['AdobeTimes']
-  }
+    families: ["AdobeTimes"],
+  },
 });
-
-
-
 
 ReactDOM.render(
   <>
-    <div className="section" id="stuff" >
-      <BulletApp  />
+    <div className="section" id="stuff">
+      <BulletApp />
     </div>
     <div className="container" id="footer">
-      <div>If you have feedback, submit
-          an <a href='https://github.com/AF-VCD/pdf-bullets/issues'>issue</a>
-          or a <a href="https://github.com/AF-VCD/pdf-bullets/pulls">pull request</a>
+      <div>
+        If you have feedback, submit an{" "}
+        <a href="https://github.com/AF-VCD/pdf-bullets/issues">issue</a>
+        or a{" "}
+        <a href="https://github.com/AF-VCD/pdf-bullets/pulls">pull request</a>
       </div>
-      <div>This site utilizes PDF.JS (pdf import), react-table (spreadsheet), draft-js (editor), the DataMuse API (thesaurus), and Bulma (CSS).</div>
-      <div>This site has basic analytics to track the total number of visits to the page. See <a href="https://github.com/ckhordiasma/log-bullet-visitors">here</a> for details</div>
+      <div>
+        This site utilizes PDF.JS (pdf import), react-table (spreadsheet),
+        draft-js (editor), the DataMuse API (thesaurus), and Bulma (CSS).
+      </div>
+      <div>
+        This site has basic analytics to track the total number of visits to the
+        page. See{" "}
+        <a href="https://github.com/ckhordiasma/log-bullet-visitors">here</a>{" "}
+        for details
+      </div>
       <div>Maintained by Christopher Kodama </div>
     </div>
-  </>
-  , document.getElementById('root'));
+  </>,
+  document.getElementById("root")
+);
 
 incrementVisitors();
 
@@ -47,23 +52,30 @@ incrementVisitors();
 serviceWorker.register();
 
 function incrementVisitors() {
-  // code for incrementing visitor count    
+  // code for incrementing visitor count
   const xh = new XMLHttpRequest();
   xh.onreadystatechange = () => {
     if (xh.readyState === XMLHttpRequest.DONE) {
       var status = xh.status;
 
       // Not sure what status codes are acceptable.. using MDN successful responses and redirects range as a guide
-      if ((status >= 200 && status < 400)) {
+      if (status >= 200 && status < 400) {
         // The request has been completed successfully
         const count = JSON.parse(xh.response).Count;
-        console.log("The bullets site(s) have been visited " + count + " times in the last 7 days.");
-
+        console.log(
+          "The bullets site(s) have been visited " +
+            count +
+            " times in the last 7 days."
+        );
       } else {
         console.log("Visitor count increment: task failed successfully");
       }
     }
-  }
-  xh.open("POST", "https://g5z50elklh.execute-api.us-east-2.amazonaws.com/default/LogVisitors", true);
+  };
+  xh.open(
+    "POST",
+    "https://g5z50elklh.execute-api.us-east-2.amazonaws.com/default/LogVisitors",
+    true
+  );
   xh.send();
 }
